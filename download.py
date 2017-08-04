c5140c2ef5c057205014cd1cc378ef867fa27096
chendinghao <171196162@qq.com>
2017-08-04 09:01:54 +0800

    null

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 # -*- coding = UTF-8 -*-
 # author: Howard
-
+# num_retries 重试次数       user_agent 用户代理    proxy 也是代理
 import urllib2
 import urlparse
 
@@ -30,3 +30,43 @@ def download3(url,num_retries=2):
              if hasattr(e,'code') and 500<= e.code <600:
                  html = download3(url, num_retries-1)
     return html
+
+def download4(url,user_agent='wawp',num_retries=2):
+    """Download function that includes user agent support"""
+    print "downloading:",url
+    headers = {'User-agent' = user_agent}
+    request = urllib2.Request(url, headers = headers)
+    try:
+        html = urllib2.urlopen(url).read()
+    except urllib2.URLError as e:
+        print 'Download error:', e.reason
+        html = None
+        if num_retries >0:
+             if hasattr(e,'code') and 500<= e.code <600:
+                 html = download4(url, user_agent,num_retries-1)
+    return html
+
+def download5(url, user_agent='wswp', proxy=None, num_retries=2):
+    """Download function with support for proxies"""
+    print 'Downloading:', url
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
+    opener = urllib2.build_opener()
+    if proxy:
+        proxy_params = {urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_params))
+    try:
+        html = opener.open(request).read()
+    except urllib2.URLError as e:
+        print 'Download error:', e.reason
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                # retry 5XX HTTP errors
+                html = download5(url, user_agent, proxy, num_retries-1)
+    return html
+
+download = download5
+
+if __name__ == '__main__':
+    print download('http://example.webscraping.com')
